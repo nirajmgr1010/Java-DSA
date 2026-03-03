3854e7d44ed98839da54e16d6bcf7cb36b2f9cef
nirajmgr1010 <neerazmgr9@gmail.com>
2026-03-03 22:27:31 +0545

    recursion 1

---
+++ recursion/TOH.java
@@ -0,0 +1,6 @@
+
+public class TOH {
+public static void main(String[] args) {
+    
+}    
+}
---
+++ recursion/recursion1.java
@@ -0,0 +1,21 @@
+/*
+Qs.Print Numbers from 1 to 5
+*/
+class PrintNumbers{
+  
+    void Print(int n){
+       if(n>5){
+        return;
+       }
+       System.out.println(n);
+       Print(n+1);
+       
+    }
+}
+public class recursion1 {
+ public static void main(String[] args) {
+        int n= 1;
+        PrintNumbers p = new PrintNumbers();
+        p.Print(n);
+ }    
+}
