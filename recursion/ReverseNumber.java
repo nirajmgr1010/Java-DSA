2de646f6f1e773c3451efee9a8e44ac80ef16497
nirajmgr1010 <neerazmgr9@gmail.com>
2026-03-14 21:32:35 +0545

    push

--- recursion/ReverseNumber.java
+++ recursion/ReverseNumber.java
@@ -1,12 +1,24 @@
 public class ReverseNumber {
-    public static void ReverseNumber(int digit){
-         if(digit == 0){
-            return;
-         }
-          int result = ReverseNumber(digit%10);
 
+    public static void reverseNumber(int digit) {
+
+        // base case
+        if (digit == 0) {
+            return;
+        }
+
+        // print last digit
+        System.out.print(digit % 10);
+
+        // recursive call
+        reverseNumber(digit / 10);
     }
+
     public static void main(String[] args) {
-        
+
+        int number = 5736;
+
+        System.out.print("Reversed Number: ");
+        reverseNumber(number);
     }
 }
