019d4d538fb1efbb6518f3ce76008e02b4d3030d
nirajmgr1010 <neerazmgr9@gmail.com>
2026-03-18 23:11:49 +0545

    changes

--- Sorting/BubbleSort.c
+++ Sorting/BubbleSort.c
@@ -7,10 +7,32 @@ void printArray(int* A, int n){
     }
     printf("\n");
 }
+void bubbleSortAdaptive(int *A,int n){
+    int temp;
+    int isSorted = 0;
+    for (int i = 0; i < n-1 ; i++) //For number of pass
+    {
+        printf("Working on pass number %d\n",i+1);
+        isSorted = 1;
+        for(int j=0; j<n-1-i; j++){ //For comparision in each pass
+            if(A[j] > A[j+1]){
+             temp = A[j];
+             A[j] = A[j+1];
+             A[j+1] = temp;
+             isSorted = 0;
+            }
+        }
+        if(isSorted){
+            return; //Making adaptibility
+        }
+    }
+    
+}
 void bubbleSort(int *A,int n){
     int temp;
     for (int i = 0; i < n-1 ; i++) //For number of pass
     {
+        printf("Working on pass number %d\n",i+1);
         for(int j=0; j<n-1-i; j++){ //For comparision in each pass
             if(A[j] > A[j+1]){
              temp = A[j];
@@ -23,8 +45,9 @@ void bubbleSort(int *A,int n){
 }
 int main(){
   int A[] = {12,54,65,7,23,9};
+//   int A[] = {1,2,3,4,5,6};
   int n = 6;
   printArray(A,n); //Printing the array before sorting
-  bubbleSort(A,n); //Function to sort the array
+  bubbleSortAdaptive(A,n); //Function to sort the array
   printArray(A,n); //Printing the array before sorting
 }
