b8706a5e4753630f20a541a4431721ae8ee5681e
nirajmgr1010 <neerazmgr9@gmail.com>
2026-07-11 11:12:12 +0545

    Changes

---
+++ Array/longestcommonprefix.java
@@ -0,0 +1,12 @@
+
+
+public class longestcommonprefix {
+
+    public static String LongestCommonPrefix(String [] strs){
+   if(strs[0].equals(strs[1]))
+    }
+    public static void main(String[] args) {
+        String[] strs = {"flower", "flow", "flight"};
+        System.out.println(LongestCommonPrefix(strs));
+    }
+}
